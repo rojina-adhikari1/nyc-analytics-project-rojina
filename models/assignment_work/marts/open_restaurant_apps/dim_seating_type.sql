f51fcd4b366bcdb38055173edd0ff3dc2d28817a
Rojina Adhikari <baruch.rojina@gmail.com>
2026-03-13 23:05:15 +0000

    updated code as per error message

--- models/assignment_work/marts/open_restaurant_apps/dim_seating_type.sql
+++ models/assignment_work/marts/open_restaurant_apps/dim_seating_type.sql
@@ -1,37 +1,34 @@
 -- Seating type dimension for open restaurant seating applications
 WITH seating_types AS (
-   SELECT DISTINCT
-       seating_interest_sidewalk AS seating_interest,
--- Convert raw approval values to boolean (TRUE/FALSE)
+    SELECT DISTINCT
+        seating_interest,  -- staging column name
+        
+        -- Convert raw approval values to boolean (TRUE/FALSE)
         CASE 
-            WHEN approved_for_sidewalk_seating = 'Yes' THEN TRUE
+            WHEN LOWER(sidewalk_seating_approved) = 'yes' THEN TRUE
             ELSE FALSE
         END AS approved_for_sidewalk,
         
         CASE 
-            WHEN approved_for_roadway_seating = 'Yes' THEN TRUE
+            WHEN LOWER(roadway_seating_approved) = 'yes' THEN TRUE
             ELSE FALSE
         END AS approved_for_roadway
 
-    FROM {{ ref('stg_open_restaurant_applications') }} 
-      AS approved_for_roadway
-   FROM --TODO: reference the appropriate staging table!
-   WHERE seating_interest_sidewalk IS NOT NULL
+    FROM {{ ref('stg_nyc_open_restaurant_apps') }}
+    WHERE seating_interest IS NOT NULL
 ),
+
 seating_dimension AS (
-   SELECT
+    SELECT
        {{ dbt_utils.generate_surrogate_key([
            'seating_interest',
            'approved_for_sidewalk',
            'approved_for_roadway'
        ]) }} AS seating_type_key,
-
-       -- TODO: fill in the rest of this SELECT statement
-       --  based on the dimensional model!
        seating_interest,
-       approved_for_sidewalk,
-       approved_for_roadway
-   FROM seating_types
+        approved_for_sidewalk,
+        approved_for_roadway
+    FROM seating_types
 )
 
 SELECT * FROM seating_dimension
